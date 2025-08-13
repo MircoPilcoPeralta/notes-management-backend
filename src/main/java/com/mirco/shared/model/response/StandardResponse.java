b8a5166338d44de281423b3b9e2d824e42873271
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2025-08-13 18:21:35 -0400

    feat: enhance StandardResponse with Lombok annotations

--- src/main/java/com/mirco/shared/model/response/StandardResponse.java
+++ src/main/java/com/mirco/shared/model/response/StandardResponse.java
@@ -1,12 +1,16 @@
 package com.mirco.shared.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class StandardResponse {
     private Integer statusCode;
     private String message;
