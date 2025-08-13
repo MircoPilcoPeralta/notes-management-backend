494bac1dc1ed9d34942231665a11b59acdeb95c0
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2025-08-13 17:13:18 -0400

    feat: add method to find notes by SystemUser ID

--- src/main/java/com/mirco/notes/notes/model/repository/INoteRepository.java
+++ src/main/java/com/mirco/notes/notes/model/repository/INoteRepository.java
@@ -4,6 +4,9 @@ import com.mirco.notes.notes.model.entitites.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface INoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findAllBySystemUserId(Long userId);
 }
