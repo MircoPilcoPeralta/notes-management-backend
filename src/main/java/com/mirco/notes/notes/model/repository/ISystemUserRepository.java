cd091d287c353e71298bd94472d2939fa9f006ea
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2025-08-13 15:56:22 -0400

    feat: add SystemUser repository

---
+++ src/main/java/com/mirco/notes/notes/model/repository/ISystemUserRepository.java
@@ -0,0 +1,9 @@
+package com.mirco.notes.notes.model.repository;
+
+import com.mirco.notes.notes.model.entitites.SystemUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISystemUserRepository extends JpaRepository<SystemUser, Long> {
+}
