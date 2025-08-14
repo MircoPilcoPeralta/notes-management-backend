d93fccaf500fc3ca6a0437b3bfe1b0902f0824ff
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2025-08-13 22:24:48 -0400

    refactor: class NoteServiceImpl.java

--- src/main/java/com/mirco/notes/service/notes/NoteServiceImpl.java
+++ src/main/java/com/mirco/notes/service/notes/NoteServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class NoteServiceImpl implements INoteService{
+public class NoteServiceImpl implements INoteService {
 
     private final INoteRepository iNoteRepository;
     private final ISystemUserService iSystemUserService;
