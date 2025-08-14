89fb2d6972b964a5bc7b82596618be3da7ef8472
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2025-08-14 08:43:58 -0400

    feat: add method to get labels by its id

---
+++ src/main/java/com/mirco/notes/service/label/LabelServiceImpl.java
@@ -0,0 +1,21 @@
+package com.mirco.notes.service.label;
+
+import com.mirco.notes.notes.model.entitites.Label;
+import com.mirco.notes.notes.model.repository.ILabelRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LabelServiceImpl implements ILabelService {
+
+    private final ILabelRepository iLabelRepository;
+
+    public LabelServiceImpl(ILabelRepository iLabelRepository) {
+        this.iLabelRepository = iLabelRepository;
+    }
+
+    @Override
+    public Label getLabelById(Long id) {
+        return iLabelRepository.findById(id)
+                .orElse(null);
+    }
+}
