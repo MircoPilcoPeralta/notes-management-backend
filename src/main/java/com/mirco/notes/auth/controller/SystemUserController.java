0020479ac200b34f91fe70fe495657b166836de9
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2025-08-15 10:51:14 -0400

    feat: add OpenAPI annotations for user endpoints

--- src/main/java/com/mirco/notes/auth/controller/SystemUserController.java
+++ src/main/java/com/mirco/notes/auth/controller/SystemUserController.java
@@ -4,6 +4,11 @@ import com.mirco.notes.auth.model.entities.SystemUser;
 import com.mirco.notes.auth.model.response.SystemUserInfoResponse;
 import com.mirco.notes.auth.services.systemUser.ISystemUserService;
 import com.mirco.notes.shared.model.response.StandardResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User", description = "Endpoints related to retrieve information by the authenticated user.")
 public class SystemUserController {
 
     private final ISystemUserService iSystemUserService;
@@ -21,6 +27,10 @@ public class SystemUserController {
         this.iSystemUserService = iSystemUserService;
     }
 
+    @Operation(
+        summary = "Get current authenticated user info",
+        description = "Returns the profile information of the authenticated user."
+    )
     @GetMapping("/me")
     public ResponseEntity<StandardResponse<SystemUserInfoResponse>> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
         SystemUser systemUserFromDB = iSystemUserService.findSystemUserByEmail(userDetails.getUsername());
